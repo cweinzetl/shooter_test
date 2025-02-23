a778ee15cac6cb96d2a794883c41995d9886af43
Carsen Weinzetl <ctw7@posteo.net>
2025-02-23 14:32:32 -0800

    started camera controls

--- controllers/scripts/player_controller.gd
+++ controllers/scripts/player_controller.gd
@@ -4,16 +4,51 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+# mouse input
+@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
+@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
+@export var CAMERA_CONTROLLER : Camera3D
+
+var _mouse_input : bool = false
+var _mouse_rotation : Vector3
+var _rotation_input : float
+var _tilt_input : float
+
+func _unhandled_input(event):
+	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	if _mouse_input:
+		_rotation_input = -event.relative.x
+		_tilt_input = -event.relative.y
+		print(Vector2(_rotation_input, _tilt_input))
+
+func _update_camera(delta):
+	
+	# rotate camera using euler rotation
+	_mouse_rotation.x += _tilt_input * delta 
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+	
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_mouse_rotation)
+	CAMERA_CONTROLLER.rotation.z = 0.0
+	
+	_rotation_input = 0.0
+	_tilt_input = 0.0
+	
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# Camera
+	_update_camera(delta)
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
